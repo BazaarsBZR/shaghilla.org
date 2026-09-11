ffaa34108c5fdf9437ab8914cbb33bed02c2b81e
Bazaars <102147094+BazaarsBZR@users.noreply.github.com>
2026-09-11 07:28:30 +0100

    Reduce public page cold-start work

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,6 +1,17 @@
 <?php
 
-return [
+$providers = [
     App\Providers\AppServiceProvider::class,
-    App\Providers\Filament\AdminPanelProvider::class,
 ];
+
+$requestPath = parse_url($_SERVER['REQUEST_URI'] ?? '/', PHP_URL_PATH) ?: '/';
+$needsAdminPanel = PHP_SAPI === 'cli'
+    || str_starts_with($requestPath, '/admin')
+    || str_starts_with($requestPath, '/livewire')
+    || str_starts_with($requestPath, '/filament');
+
+if ($needsAdminPanel) {
+    $providers[] = App\Providers\Filament\AdminPanelProvider::class;
+}
+
+return $providers;
